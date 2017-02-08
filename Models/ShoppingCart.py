8009ff24128907d63e12a8d0cc4d1cf6f4ea373f
Ben Marks <onodric@yahoo.com>
2017-02-08 15:53:31 -0600

    WiP... merge master

---
+++ Models/ShoppingCart.py
@@ -0,0 +1,10 @@
+class ShoppingCart():
+    """
+    A Class defining a customer's shopping cart.
+    Initially the cart is open (or active), and will be closed 
+    upon customer payment processing.
+
+    Methods:
+
+    """
+    
