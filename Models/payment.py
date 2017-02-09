6df3a5f2cfc4f7ab568899f70d9a377dc3432a7e
Dani Adkins <danirenaeadkins12@gmail.com>
2017-02-09 11:41:20 -0600

    added test

--- Models/payment.py
+++ Models/payment.py
@@ -7,7 +7,6 @@ class Payment():
         self.__payment_type = payment_type
         self.__account_number = account_number
         self.__full_payment_info_current_account_current_user = [(1 ,"Visa", "12345678"), (2, "MC", "34534")]
-        self.__all_payment_info_current_user = []
 
     def get_payment_type(self):
         return self.__payment_type
@@ -16,17 +15,8 @@ class Payment():
         return self.__account_number
 
     def get_full_payment_info_current_account_current_user(self):
-        # self.__full_payment_info.append(self.__payment_type)
-        # self.__full_payment_info.append(self.__account_number)
         return self.__full_payment_info_current_account_current_user
 
-    # def get_all_payment_info_for_current_user(self):
-
-    def payment_info_is_posted(self):
-        pass
-
-    def payment_info_can_be_returned(self):
-        pass
 
 
 
--- Tests/payment_test.py
+++ Tests/payment_test.py
@@ -7,12 +7,8 @@ class TestPayment(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        # self.payment = Payment()
-        self.juan_payment_current_account = Payment(1, "Visa", "12345678")
-        self.taylor_payment_current_account = Payment(2, "Mastercard", "87654321")
-        self.juan_payment_all_accounts =
-        self.taylor_payment_current_accounts =
-
+        self.juan_payment = Payment(1, "Visa", "12345678")
+        self.taylor_payment = Payment(2, "Mastercard", "87654321")
 
     def test_user_payment_is_a_user_payment(self):
         self.assertIsInstance(self.juan_payment, Payment)
@@ -25,20 +21,6 @@ class TestPayment(unittest.TestCase):
 
     def test_can_get_full_payment_info_current_account_and_current_user(self):
         self.assertIn((1, "Visa", "12345678"), self.juan_payment.get_full_payment_info_current_account_current_user())
-
-    def test_can_get_all_accounts_for_current_user(self):
-        self.
-
-
-
-    # def test_payment_is_posted(self):
-    #     payment = payment_is_posted(self.juan_payment)
-    #     self.assertTrue()
-
-
-    # def test_user_payment_is_returning_as_a_tuple(self):
-    #     payment = self.juan_payment.get_full_payment_info()
-    #     self.assertTrue(payment, get_full_payment_info(self.juan_payment))
 
 if __name__ == "__main__":
     unittest.main()
