be8603e55124643462ee22744f28a6cfc65bb99f
john pinkston <pinkstonjack2@gmail.com>
2017-02-09 10:53:45 -0600

    added test for current customer, added active attribute, added method for returning current customer

--- Models/Customers.py
+++ Models/Customers.py
@@ -1,15 +1,18 @@
 class Customer():
 
-	def __init__(self, name, address, state, postal, phone):
+	def __init__(self, name, address, city, state, postal, phone):
 		self.name = name
 		self.address = address
+		self.city = city
 		self.state = state
 		self.postal = postal
 		self.phone = phone
+		self.active = False
 
-	def get_name(self):
-		return self.name
+	def get_current_customer(self):
+		return [(self.name, self.address, self.city, self.state, self.postal, self.phone, self.active)]
 
-	def set_name(self):
-		return self.address
+	
+
+	
 
---
+++ Tests/customer_test.py
@@ -0,0 +1,43 @@
+import unittest
+import sys
+sys.path.append("../")
+from Models.customers import Customer
+
+class TestCustomer(unittest.TestCase):
+	"""
+	customer should have a:
+	name
+	street address, city, state, zipcode
+	phone #
+	"""
+	@classmethod
+	def setUpClass(self):
+		self.me = Customer("Me", "500 interstate blvd", "Nashville", "TN", "11111", "666-6666")
+
+	def test_customer_is_instance(self):
+		self.assertIsInstance(self.me, Customer)
+
+	def test_can_return_current_customer(self):
+		current_customer = Customer.get_current_customer(self.me)
+		self.assertEqual(self.me.name, current_customer[0][0])
+		self.assertEqual(self.me.address, current_customer[0][1])
+		self.assertEqual(self.me.city, current_customer[0][2])
+		self.assertEqual(self.me.state, current_customer[0][3])
+		self.assertEqual(self.me.postal, current_customer[0][4])
+		self.assertEqual(self.me.phone, current_customer[0][5])
+		self.assertEqual(self.me.active, current_customer[0][6])
+
+
+	def test_customer_has_attribute(self):
+		self.assertEqual("Me", self.me.name)
+		self.assertEqual("500 interstate blvd", self.me.address)
+		self.assertEqual("Nashville", self.me.city)
+		self.assertEqual('TN', self.me.state)
+		self.assertEqual("11111", self.me.postal)
+		self.assertEqual("666-6666", self.me.phone)
+		self.assertEqual(False, self.me.active)
+
+
+
+if __name__ == "__main__":
+	unittest.main()
