4e09892885d588535e6dd9c95d5beca355af424a
Samuel Phillips <samphillips1879@gmail.com>
2017-02-09 10:35:27 -0600

    menu

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ running = True
 
 while running:
     selected_option = input("""
-*********************************************************\n**  Welcome to Bangazon! Command Line Ordering System  **\n*********************************************************\n
+*********************************************************
+**  Welcome to Bangazon! Command Line Ordering System  **
+*********************************************************
 1. Create a customer account
 2. Choose active customer
 3. Create a payment option
@@ -14,19 +16,7 @@ while running:
 5. Complete an order
 6. See product popularity
 7. Leave Bangazon!
-
-
-
-
-
-
-
-
-
-
-
-
-""")
+>""")
 
     if selected_option == "1":
         print("Create a customer account")
