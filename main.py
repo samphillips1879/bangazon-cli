897267ac605598c981cd4d4d3d012c3e5b1a7128
Samuel Phillips <samphillips1879@gmail.com>
2017-02-09 10:10:02 -0600

    main menu display

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+
+running = True
+
+while running:
+    selected_option = input("""*********************************************************\n**  Welcome to Bangazon! Command Line Ordering System  **\n*********************************************************\n
+1. Create a customer account
+2. Choose active customer
+3. Create a payment option
+4. Add product to shopping cart
+5. Complete an order
+6. See product popularity
+7. Leave Bangazon!
+""")
+
+    if selected_option == "1":
+        print("customer yo")
+
+
+
+
+
+
+    running = False
